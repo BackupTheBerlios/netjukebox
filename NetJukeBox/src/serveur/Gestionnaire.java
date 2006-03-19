2318503577dc38017d2a30d4f821509065b6745c
pbollard <pbollard>
2006-03-19 21:47:17 +0000

    Diffusion document (streaming)

--- NetJukeBox/src/serveur/Gestionnaire.java
+++ NetJukeBox/src/serveur/Gestionnaire.java
@@ -1,7 +1,79 @@
 package serveur;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.Vector;
+
+import org.apache.xmlrpc.XmlRpc;
+import org.apache.xmlrpc.XmlRpcClient;
+import org.apache.xmlrpc.XmlRpcException;
+
 public class Gestionnaire {
-	public String sayHello(String name) {
-		return "Bonjour " + name;
+
+	private XmlRpcClient streaming;
+
+	public Gestionnaire() {
+		try {
+			// Utilise lengthpilote Sax d'Apache Xerces
+			XmlRpc.setDriver("org.apache.xerces.parsers.SAXParser");
+
+			// Creation du client & identification du serveur
+			streaming = new XmlRpcClient("http://localhost:10000");
+		} catch (ClassNotFoundException e) {
+			System.out.println("Impossible de localiser le pilote Sax");
+		} catch (MalformedURLException e) {
+			System.out.println("URL non conforme au format du serveur XML-RPC");
+		}
+
 	}
+
+	/**
+	 * Lancer la diffusion d'un document
+	 * @param filename
+	 * @return
+	 */
+	public boolean startDiffusionDocument(String filename) {
+		Vector params = new Vector();
+		params.addElement(filename);
+		try {
+			String result = (String) streaming.execute(
+					"Streaming.startDiffusionDocument", params);
+			System.out.println("Réponse du serveur : " + result);
+		} catch (MalformedURLException e) {
+			System.out.println("URL non conforme au format du serveur XML-RPC");
+		} catch (IOException e) {
+			System.out.println("IO Exception: " + e.getMessage());
+		} catch (XmlRpcException e) {
+			System.out.println("Exception E/S : " + e.getMessage());
+		}
+
+		return true;
+	}
+
+	/**
+	 * Stopper la diffusion d'un document
+	 * @param filename
+	 * @return
+	 */
+	public boolean stopDiffusionDocument(String filename) {
+		Vector params = new Vector();
+		params.addElement(filename);
+		try {
+			String result = (String) streaming.execute(
+					"Streaming.stopDiffusionDocument", params);
+			System.out.println("Réponse du serveur : " + result);
+		} catch (MalformedURLException e) {
+			System.out.println("URL non conforme au format du serveur XML-RPC");
+		} catch (IOException e) {
+			System.out.println("IO Exception: " + e.getMessage());
+		} catch (XmlRpcException e) {
+			System.out.println("Exception E/S : " + e.getMessage());
+		}
+
+		return true;
+	}
+	
+	/*
+	 * public String sayHello(String name) { return "Bonjour " + name; }
+	 */
 }
