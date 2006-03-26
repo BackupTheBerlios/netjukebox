bf39429459d0be089ee13461504b19c4b14fada2
dominique <dominique>
2006-03-26 17:21:27 +0000

    Les premières méthodes

--- NetJukeBox/src/serveur/DOCUMENT.java
+++ NetJukeBox/src/serveur/DOCUMENT.java
@@ -8,23 +8,87 @@ public class DOCUMENT {
 	public String Titre;
 	public String Source;
 	public int Duree;
-	public java.util.Date Date_Creation;
+	public int Jour;
+	public int Mois;
+	public int Annee;
 	public String Langue;
 	public String Genre;
 	public String Fichier;
+	
 	public String Etat;
 
-	//	Création du vecteur d'Id de Document
+	//	Création du vecteur d'Id de programme et initialisation du compteur de verrou
 	Vector vecteur_verrou = new Vector();
 	int compteur_verrou = 0;
 	
+	// A quoi ça sert ???
 	public java.util.Collection pROGRAMME = new java.util.TreeSet();
 	public Contrat contrat;
     
-	public void Creer(String Id_Doc, String Titre, java.util.Date Date_Creation, String Source, String Langue, String Genre, String Fichier) {        
-        // your code here
+	public void Creer(String Id_Doc, String Titre, int Duree, int Jour, int Mois, int Annee, String Source, String Langue, String Genre, String Fichier) {        
+		this.Id_Doc = Id_Doc;
+		this.Titre = Titre;
+		this.Jour = Jour;
+		this.Mois = Mois;
+		this.Annee = Annee;
+		this.Source = Source;
+		this.Langue = Langue;
+		this.Genre = Genre;
+		this.Fichier = Fichier;
+		this.Duree = Duree;
+    } 
+	
+	public String GetId_Doc() {        
+		return Id_Doc;
     } 
 
+	public String GetTitre() {        
+			return Titre;
+	} 
+
+	 public int GetDuree() {
+		 return Duree;
+	 }
+
+	public String GetSource() {        
+		return Source;
+    } 
+
+	public String GetLangue() {        
+		return Langue;
+    } 
+
+	public String GetGenre() {        
+		return Genre;
+    } 
+	
+	public void PoserVerrou(String Id_Prog) {        
+		vecteur_verrou.addElement(Id_Prog);
+		compteur_verrou = compteur_verrou + 1;
+		System.out.println("Document verrouillé : " + Id_Doc);
+		System.out.println("Le compteur de verrou = " + compteur_verrou);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public String GetEtat() {        
+        // your code here
+        return null;
+    }
+	
+	public java.util.Date GetDate_Céation() {        
+        // your code here
+		return null;
+    }	
+	
+	
 	public void NommerFichier(String Fichier) {        
         // your code here
     } 
@@ -75,56 +139,12 @@ public class DOCUMENT {
         // your code here
     } 
 
-	public String GetId_Doc() {        
-        // your code here
-        return Id_Doc;
-    } 
-
-	public String GetTitre() {        
-        // your code here
-        return null;
-    } 
-
-	public java.util.Date GetDate_Céation() {        
-        // your code here
-        return null;
-    } 
-
-	public String GetSource() {        
-        // your code here
-        return null;
-    } 
-
-	public int GetDuree() {        
-        // your code here
-        return 0;
-    } 
-
-	public String GetEtat() {        
-        // your code here
-        return null;
-    } 
-
-	public String GetLangue() {        
-        // your code here
-        return null;
-    } 
-
-	public String GetGenre() {        
-        // your code here
-        return null;
-    } 
-
+	
 	public void DeverouillerDocument(String Id_Doc) {        
         // your code here
     } 
 	
-	public void PoserVerrou(String Id_Prog) {        
-		vecteur_verrou.addElement(Id_Prog);
-		compteur_verrou = compteur_verrou + 1;
-		System.out.println("Document verrouillé : " + Id_Doc);
-		System.out.println("Le compteur de verrou = " + compteur_verrou);
-    }
+	
 
 	public void CompterVerrouProgramme(int Verrou) {        
         // your code here
