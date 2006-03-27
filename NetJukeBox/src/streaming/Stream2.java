7275903f69d4a41039047358cd420cae70185bc4
pbollard <pbollard>
2006-03-27 19:05:55 +0000

    test SupInfo

--- NetJukeBox/src/streaming/Stream2.java
+++ NetJukeBox/src/streaming/Stream2.java
@@ -1,18 +1,65 @@
 package streaming;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import javax.media.Format;
+import javax.media.Manager;
 import javax.media.MediaLocator;
+import javax.media.NoProcessorException;
+import javax.media.Processor;
+import javax.media.control.TrackControl;
+import javax.media.format.UnsupportedFormatException;
+import javax.media.protocol.ContentDescriptor;
+import javax.media.protocol.DataSource;
+import javax.media.rtp.InvalidSessionAddressException;
+import javax.media.rtp.RTPManager;
+import javax.media.rtp.SendStream;
+import javax.media.rtp.SessionAddress;
+//import com.sun.media.protocol.avi.DataSource;
+
 
 public class Stream2 {
 
 	// Attributs
+	private String filename;
+	private MediaLocator locator;
+	private String ipAddress;
+	private int portBase;
+	private Processor processor = null;
+	private RTPManager rtpMgrs[];
+	private DataSource dataOutput = null;
+	private Processor FichierCessor;
 
-	// Constructeur
-	public Stream2() {
+	public Stream2(String filename, String ipAddress, int portBase) {
+
+		this.filename = filename;
+		this.locator = new MediaLocator(filename);
+		this.ipAddress = ipAddress;
+		this.portBase = portBase;
+		
+		//Declaration du Processor
+		this.FichierCessor = null;
+		try {
+			// Creation du Processor à partir du Medialocator
+			this.FichierCessor = Manager.createProcessor(this.locator);
+			this.configure(this.FichierCessor);
+			this.SetSupportedFormat(this.FichierCessor);
+			this.realize(this.FichierCessor);
+			this.Demarre(this.FichierCessor);
+		}
+
+		catch (IOException e) {
+			System.out.println("Erreur : " + e.getMessage());
+		} catch (NoProcessorException e) {
+			System.out.println("Erreur : " + e.getMessage());
+		}
 	}
 
 	public void debut() {
 		// Entrée de l'adresse du fichier
-		String FichierAdresse = "file://d:/test.avi";
+		String FichierAdresse = "file://file.mp3";
 
 		// Création du MédiaLocator à partir de ce fichier
 		MediaLocator FichierLocator = new MediaLocator(FichierAdresse);
@@ -24,252 +71,164 @@ public class Stream2 {
 			FichierCessor = Manager.createProcessor(FichierLocator);
 		}
 
-		catch (IOException e){
+		catch (IOException e) {
 			System.out.println("Erreur : " + e.getMessage());
-		}
-		catch (NoProcessorException e){
+		} catch (NoProcessorException e) {
 			System.out.println("Erreur : " + e.getMessage());
 		}
 	}
-	
+
 	public Processor configure(Processor p) {
 
-	       //Attendre tant que le Processor n'est pas configuré.
-	       while(p.getState() < Processor.Configured) {
-	              //Configuration du Processor
-	              p.configure();
-	       }
-	       return p;
+		// Attendre tant que le Processor n'est pas configuré.
+		while (p.getState() < Processor.Configured) {
+			// Configuration du Processor
+			p.configure();
+		}
+		return p;
 	}
-	
+
 	public void SetSupportedFormat(Processor p) {
 
-	       //On met la description du contenu de sortie à RAW_RTP
-	       //pour limiter les formats supportés
+		// On met la description du contenu de sortie à RAW_RTP
+		// pour limiter les formats supportés
+		ContentDescriptor cd = new ContentDescriptor(ContentDescriptor.RAW_RTP);
+		p.setContentDescriptor(cd);
 
-	       ContentDescriptor cd = new ContentDescriptor(ContentDescriptor.RAW_RTP);
-	       p.setContentDescriptor(cd);
+		// On obtient les différentes pistes du processor
+		TrackControl track[] = p.getTrackControls();
 
-	       //On obtient les différentes pistes du processor
-	       TrackControl track[] = p.getTrackControls();
-	       
-	       for(int i = 0 ; i < track.length ; i++) {
+		for (int i = 0; i < track.length; i++) {
 
-	              //on obtient les formats supportés pour cette piste
-	              Format suppFormats[] = track[i].getSupportedFormats();
+			// on obtient les formats supportés pour cette piste
+			Format suppFormats[] = track[i].getSupportedFormats();
 
-	              //Si il y a au moins un format supporté
-	              //alors on met le premier
+			// Si il y a au moins un format supporté
+			// alors on met le premier
 
-	              if(suppFormats.length > 0)  track[i].setFormat(suppFormats[0]);
-	              else track[i].setEnabled(false);
-	       }
+			if (suppFormats.length > 0)
+				track[i].setFormat(suppFormats[0]);
+			else
+				track[i].setEnabled(false);
+		}
 	}
-	
+
 	public Processor realize(Processor p) {
 
-	       //Attendre tant que le Processor n'est pas réalisé.
-	       while(p.getState() < Processor.Realized) {
-	              //Réalisation du Processor
-	              p.realize();
-	       }
-	       return p;
+		// Attendre tant que le Processor n'est pas réalisé.
+		while (p.getState() < Processor.Realized) {
+			// Réalisation du Processor
+			p.realize();
+		}
+		return p;
 	}
-	
+
 	public void Demarre(Processor p) {
-	       //Demarrage du Processor
-	       p.start();
+		// Demarrage du Processor
+		p.start();
 	}
-	
-	public void createDataSink(Processor p) {
 
-	       //Creation du DataSource correspondant au Processor
-	       DataSource WebcamSource = p.getDataOutput();
-
-	       //Adresse de Destination
-	       String OutputAddress = "rtp://192.168.3.6:22224/video/1";
-
-	       //Creation du MediaLocator pour l'Adresse de destination
-	       MediaLocator OutputLocator = new MediaLocator(OutputAddress);
-
-	       try {
-
-	              //Creation du DataSink
-	              DataSink OutputSink =Manager.createDataSink(WebcamSource,OutputLocator);
-
-	              //Ouverture du DataSink
-	              OutputSink.open();
-
-	              //Demarrage du DataSink
-	              OutputSink.start();
-	              System.out.println("Started");
-
-	       }
-	       catch(IOException e) {
-	       }
-	       catch(NoDataSinkException e) {
-	       }
-	}
-	
 	public void createRTPManager(Processor p) {
 
-	       //Creation du DataSource correspondant au Processor
-	       DataSource OutputSource = p.getDataOutput();
+		// Creation du DataSource correspondant au Processor
+		DataSource OutputSource = p.getDataOutput();
 
-	       //Nouvelle Instance d'un RTPManager
-	       RTPManager rtpm = RTPManager.newInstance();
+		// Nouvelle Instance d'un RTPManager
+		RTPManager rtpm = RTPManager.newInstance();
 
-	       try {
+		try {
 
-	              //Création d'une SessionAddress
-	              //correspondant à l'adresse locale
+			// Création d'une SessionAddress
+			// correspondant à l'adresse locale
 
-	              SessionAddress localaddr = new SessionAddress(InetAddress.getLocalHost(),40000);
+			SessionAddress localaddr = new SessionAddress(InetAddress
+					.getLocalHost(), 40000);
 
-	              //Initialisation du RTPManager
-	              //à partir de la SessionAddresse locale
+			// Initialisation du RTPManager
+			// à partir de la SessionAddresse locale
 
-	              rtpm.initialize(localaddr);
-	              
-	              //Création d'une SessionAddress
-	              // correspondant à l'adresse de destination       
+			rtpm.initialize(localaddr);
 
-	              SessionAddress destaddr = new SessionAddress(InetAddress.getByName("192.168.3.6"),22224);
+			// Création d'une SessionAddress
+			// correspondant à l'adresse de destination
 
-	              //Ajout de cette SessionAddress dans le RTPManager
-	              rtpm.addTarget(destaddr);
+			SessionAddress destaddr = new SessionAddress(InetAddress
+					.getByName("127.0.0.1"), 22224);
 
-	              //Creation d'un SendStream à partir du DataSource
-	              SendStream ss2 = rtpm.createSendStream(OutputSource,0);
+			// Ajout de cette SessionAddress dans le RTPManager
+			rtpm.addTarget(destaddr);
 
-	              //Demarrage du SendStream
-	              ss2.start();
-	              System.out.println("Started");
+			// Creation d'un SendStream à partir du DataSource
+			SendStream ss2 = rtpm.createSendStream(OutputSource, 0);
 
-	       }
-	       catch(UnknownHostException e) {
-	       }     
-	       catch(IOException e) {
-	       }
-	       catch(InvalidSessionAddressException e) {
-	       }
+			// Demarrage du SendStream
+			ss2.start();
+			System.out.println("Started");
 
-	       catch(UnsupportedFormatException e) {
-	       }
+		} catch (UnknownHostException e) {
+		} catch (IOException e) {
+		} catch (InvalidSessionAddressException e) {
+		}
+
+		catch (UnsupportedFormatException e) {
+		}
 
 	}
-	
-	public void createRTPManager2(Processor p)
 
-	{
+	public void createRTPManager2(Processor p) {
 
-	       //Creation du DataSource correspondant au Processor
+		// Creation du DataSource correspondant au Processor
+		DataSource OutputSource = p.getDataOutput();
 
-	       DataSource OutputSource = p.getDataOutput();
+		// Nouvelle Instance d'un RTPManager
+		RTPManager rtpm = RTPManager.newInstance();
 
-	             
+		try {
 
-	       //Nouvelle Instance d'un RTPManager
+			// Création d'une SessionAddress
+			// correspondant à l'adresse locale
+			SessionAddress localaddr = new SessionAddress(InetAddress
+					.getLocalHost(), 40000);
+			rtpm.initialize(localaddr);
 
-	       RTPManager rtpm = RTPManager.newInstance();
+			// Création d'une SessionAddress
+			// correspondant à la première adresse de destination
+			SessionAddress destaddr1 = new SessionAddress(InetAddress
+					.getByName("192.168.3.6"), 22224);
 
-	             
+			// Ajout de la première SessionAddress dans le RTPManager
+			rtpm.addTarget(destaddr1);
 
-	       try
+			// Creation d'un premier SendStream à partir du DataSource
+			// Ce SendStream enverra le flux
+			// à la première adresse de destination
+			SendStream ss = rtpm.createSendStream(OutputSource, 0);
 
-	       {
+			// Demarrage du premier SendStream
+			ss.start();
 
-	              //Création d'une SessionAddress
+			// Création d'une SessionAddress
+			// correspondant à la seconde adresse de destination
+			SessionAddress destaddr2 = new SessionAddress(InetAddress
+					.getByName("192.168.3.2"), 22224);
 
-	              //     correspondant à l'adresse locale
+			// Ajout de la seconde SessionAddress dans le RTPManager
+			rtpm.addTarget(destaddr2);
 
-	              SessionAddress localaddr = new SessionAddress
+			// Creation d'un second SendStream à partir du DataSource
+			// Ce SendStream enverra le flux
+			// à la première adresse de destination
+			SendStream ss2 = rtpm.createSendStream(OutputSource, 0);
 
-	                     (InetAddress.getLocalHost(),40000);
+			// Demarrage du second SendStream
+			ss2.start();
+			System.out.println("Started");
 
-	              rtpm.initialize(localaddr);
-
-	                    
-
-	              //Création d'une SessionAddress
-
-	              //correspondant à la première adresse de destination
-
-	              SessionAddress destaddr1 = new SessionAddress
-
-	                     (InetAddress.getByName("192.168.3.6"),22224);
-
-	              //Ajout de la première SessionAddress dans le RTPManager
-
-	              rtpm.addTarget(destaddr1);
-
-	              //Creation d'un premier SendStream à partir du DataSource
-
-	              //     Ce SendStream enverra le flux
-
-	              //     à la première adresse de destination
-
-	              SendStream ss = rtpm.createSendStream(OutputSource,0);
-
-	              //Demarrage du premier SendStream
-
-	              ss.start();
-
-	      
-
-	              //Création d'une SessionAddress
-
-	              //correspondant à la seconde adresse de destination     
-
-	              SessionAddress destaddr2 = new SessionAddress
-
-	                     (InetAddress.getByName("192.168.3.2"),22224);
-
-	              //Ajout de la seconde SessionAddress dans le RTPManager
-
-	              rtpm.addTarget(destaddr2);
-
-	              //Creation d'un second SendStream à partir du DataSource
-
-	              //     Ce SendStream enverra le flux
-
-	              //     à la première adresse de destination
-
-	              SendStream ss2 = rtpm.createSendStream(OutputSource,0);
-
-	              //Demarrage du second SendStream
-
-	              ss2.start();
-
-	                    
-
-	              System.out.println("Started");
-
-	       }
-
-	       catch(UnknownHostException e)
-
-	       {
-
-	       }     
-
-	       catch(IOException e)
-
-	       {
-
-	       }
-
-	       catch(InvalidSessionAddressException e)
-
-	       {
-
-	       }
-
-	       catch(UnsupportedFormatException e)
-
-	       {
-
-	       }
+		} catch (UnknownHostException e) {
+		} catch (IOException e) {
+		} catch (InvalidSessionAddressException e) {
+		} catch (UnsupportedFormatException e) {
+		}
 
 	}
 }
