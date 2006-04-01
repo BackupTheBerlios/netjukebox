edf1b5a6ec28b3ff0aa68b1feb794d678f8ad159
pbollard <pbollard>
2006-04-01 19:40:17 +0000

    Début de l'amélioration de l'architecture

---
+++ NetJukeBox/src/streaming2/Stream.java
@@ -0,0 +1,120 @@
+package streaming2;
+
+import java.awt.Dimension;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Vector;
+
+import javax.media.Codec;
+import javax.media.Control;
+import javax.media.Controller;
+import javax.media.ControllerClosedEvent;
+import javax.media.ControllerEvent;
+import javax.media.ControllerListener;
+import javax.media.Format;
+import javax.media.MediaLocator;
+import javax.media.NoProcessorException;
+import javax.media.Owned;
+import javax.media.Player;
+import javax.media.Processor;
+import javax.media.control.QualityControl;
+import javax.media.control.TrackControl;
+import javax.media.format.UnsupportedFormatException;
+import javax.media.format.VideoFormat;
+import javax.media.protocol.ContentDescriptor;
+import javax.media.protocol.DataSource;
+import javax.media.protocol.PushBufferDataSource;
+import javax.media.protocol.PushBufferStream;
+import javax.media.rtp.InvalidSessionAddressException;
+import javax.media.rtp.RTPManager;
+import javax.media.rtp.SendStream;
+import javax.media.rtp.SessionAddress;
+import javax.media.rtp.rtcp.SourceDescription;
+
+/**
+ * Flux de diffusion
+ * @author Philippe Bollard
+ */
+public class Stream {
+
+	private String nomFlux;
+	private int portBase;
+	private Vector auditeurs;
+	private RTPManager rtpManager=null;
+	
+	private Processor processor = null;
+	private RTPManager rtpMgrs[];
+	private DataSource dataOutput = null;
+
+	/**
+	 * Crée un flux de diffusion
+	 * @param nomFlux
+	 * @param portBase
+	 */
+	public Stream(String nomFlux, int portBase) {
+
+		this.nomFlux = nomFlux;
+		this.portBase = portBase;
+		
+		//on ajoute un premier auditeur local
+		auditeurs.addElement("127.0.0.1");
+	}
+	
+	/**
+	 * Ajoute un auditeur et lance la session correspondante si diffusion en cours
+	 * @param ip
+	 */
+	public void ajouterAuditeur(String ip) {
+		Auditeur auditeur = new Auditeur(ip);
+
+		//Si le flux est en diffusion, on lance la session pour le nouvel auditeur
+		if (rtpManager!=null) {
+			try {
+				SessionAddress dest = new SessionAddress(InetAddress.getByName(ip),this.portBase+2);
+				rtpManager.addTarget(dest);
+				auditeur.setFlux(rtpManager.createSendStream(dataOutput,0));
+				auditeur.startFlux();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		auditeurs.addElement(auditeur);
+	}
+	
+	/**
+	 * Retire un auditeur et stoppe la session si diffusion en cours
+	 * @param ip
+	 */
+	public void retirerAuditeur(String ip) {
+		
+		//On cherche l'auditeur
+		int indice=0;
+		while (indice<auditeurs.size() && ((Auditeur)auditeurs.elementAt(indice)).getIp()!=ip) indice++;
+		
+		//Si l'auditeur a été trouvé
+		if (indice<auditeurs.size()) {
+			((Auditeur)auditeurs.elementAt(indice)).stopFlux();
+			auditeurs.removeElementAt(indice);
+		}
+		else {
+			System.out.println("ERREUR: Auditeur "+ip+" introuvable. Retrait impossible.");
+		}
+		
+	}
+
+	/**
+	 * Diffuse le fichier donné sur le flux
+	 * @param filename
+	 */
+	public void diffuser(String filename) {
+		MediaLocator locator = new MediaLocator(filename);
+	}
+
+	/**
+	 * Stoppe la diffusion du flux
+	 */
+	public void stopper() {
+	}
+}
---
+++ NetJukeBox/src/streaming2/Streaming.java
@@ -0,0 +1,33 @@
+package streaming2;
+
+import org.apache.xmlrpc.*;
+
+public class Streaming {
+
+	public static void main(String[] args) {
+		if (args.length < 1) {
+			System.out.println("Usage: java streaming [portXMLRPC] [portBase]");
+			System.exit(-1);
+		}
+
+		try {
+
+			// Utilise le pilote Sax d'Apache Xerces
+			XmlRpc.setDriver("org.apache.xerces.parsers.SAXParser");
+
+			// Démarre le serveur
+			System.out.println("Démarrage du serveur XML-RPC...");
+			WebServer server = new WebServer(Integer.parseInt(args[0]));
+
+			// Enregistre la classe du gestionnaire
+			server.addHandler("Gestionnaire", new Gestionnaire(Integer.parseInt(args[1])));
+			System.out.println("En attente de requête ...");
+
+			server.start();
+		} catch (ClassNotFoundException e) {
+			System.out.println("Impossible de localiser le pilote SAX");
+		} catch (Exception e) {
+			System.out.println("Impossible de démarrer le serveur : "+ e.getMessage());
+		}
+	}
+}
