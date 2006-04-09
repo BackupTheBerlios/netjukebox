868ee9305e1c81ce0d71d7472f8661c90ecc94d7
pbollard <pbollard>
2006-04-09 20:07:13 +0000

    Remise en forme

--- NetJukeBox/src/proto/serveur/Document.java
+++ NetJukeBox/src/proto/serveur/Document.java
@@ -2,161 +2,289 @@ package proto.serveur;
 
 import java.util.Vector;
 
-
+/**
+ * Document mis à disposition dans le NetJukeBox
+ */
 public class Document {
 
-	public String Id_Doc;
-	public String Titre;
-	public String Source;
-	public int Duree;
-	public int Jour;
-	public int Mois;
-	public int Annee;
-	public String Langue;
-	public String Genre;
-	public String Fichier;
-	
-	public String Etat;
+// ATTRIBUTS DU DOCUMENT
+//********************************************
 
-	//	Création du vecteur d'Id de programme et initialisation du compteur de verrou
-	Vector vecteur_verrou = new Vector();
-	int compteur_verrou = 0;
-	
+	/**
+	 * Identifiant du document
+	 */
+	private String id;
+
+	/**
+	 * Titre du document
+	 */
+	private String titre;
+
+	/**
+	 * Source du document
+	 */
+	private String source;
+
+	/**
+	 * Durée du document
+	 */
+	private int duree;
+
+	/**
+	 * Jour de parution du document
+	 */
+	private int jour;
+
+	/**
+	 * Mois de parution du document
+	 */
+	private int mois;
+
+	/**
+	 * Année de parution du document
+	 */
+	private int annee;
+
+	/**
+	 * Langue du document
+	 */
+	private String langue;
+
+	/**
+	 * Genre du document
+	 */
+	private String genre;
+
+	/**
+	 * Nom du fichier correspondant au document
+	 */
+	private String fichier;
+
+	/**
+	 * Etat du document
+	 */
+	private String etat;
+
+	/**
+	 * Liste de verrous sur le document
+	 */
+	private Vector verrous = new Vector();
+	private int compteur_verrou = 0;
+
 	// A quoi ça sert ???
-	public java.util.Collection pROGRAMME = new java.util.TreeSet();
-	public Contrat contrat;
-    
-	public void Creer(String Id_Doc, String Titre, int Duree, int Jour, int Mois, int Annee, String Source, String Langue, String Genre, String Fichier) {        
-		this.Id_Doc = Id_Doc;
-		this.Titre = Titre;
-		this.Jour = Jour;
-		this.Mois = Mois;
-		this.Annee = Annee;
-		this.Source = Source;
-		this.Langue = Langue;
-		this.Genre = Genre;
-		this.Fichier = Fichier;
-		this.Duree = Duree;
-    } 
+	private java.util.Collection programmes = new java.util.TreeSet();
+
+	private Contrat contrat;
+
+// CONSTRUCTEUR
+//********************************************
 	
-	public String GetId_Doc() {        
-		return Id_Doc;
-    } 
-
-	public String GetTitre() {        
-			return Titre;
-	} 
-
-	 public int GetDuree() {
-		 return Duree;
-	 }
-
-	public String GetSource() {        
-		return Source;
-    } 
-
-	public String GetLangue() {        
-		return Langue;
-    } 
-
-	public String GetGenre() {        
-		return Genre;
-    } 
-	
-	public void PoserVerrou(String Id_Prog) {        
-		vecteur_verrou.addElement(Id_Prog);
-		compteur_verrou = compteur_verrou + 1;
-		System.out.println("Document verrouillé : " + Id_Doc);
-		System.out.println("Le compteur de verrou = " + compteur_verrou);
+	public Document() {
+		
 	}
 	
-	public void DeverouillerDocument(String Id_Prog) {        
-		vecteur_verrou.remove(Id_Prog);
-		compteur_verrou = compteur_verrou - 1;
-		System.out.println("Document déverrouillé : " + Id_Doc);
+	public Document(String id, String titre, int duree, int jour,
+			int mois, int annee, String source, String langue, String genre,
+			String fichier) {
+		super();
+		this.id = id;
+		this.titre = titre;
+		this.jour = jour;
+		this.mois = mois;
+		this.annee = annee;
+		this.source = source;
+		this.langue = langue;
+		this.genre = genre;
+		this.fichier = fichier;
+		this.duree = duree;
+	}
+
+// METHODES STATIQUES
+//********************************************
+
+// METHODES DYNAMIQUES
+//********************************************
+
+	public void Creer(String Id_Doc, String Titre, int Duree, int Jour,
+			int Mois, int Annee, String Source, String Langue, String Genre,
+			String Fichier) {
+		this.id = Id_Doc;
+		this.titre = Titre;
+		this.jour = Jour;
+		this.mois = Mois;
+		this.annee = Annee;
+		this.source = Source;
+		this.langue = Langue;
+		this.genre = Genre;
+		this.fichier = Fichier;
+		this.duree = Duree;
+	}
+
+	/**
+	 * Retourne l'identifiant du document
+	 * @return String
+	 */
+	public String getId() {
+		return id;
+	}
+
+	/**
+	 * Retourne le titre du document
+	 * @return String
+	 */
+	public String getTitre() {
+		return titre;
+	}
+
+	/**
+	 * Retourne la durée du document
+	 * @return int
+	 */
+	public int getDuree() {
+		return duree;
+	}
+
+	/**
+	 * Retourne la source du document
+	 * @return String
+	 */
+	public String getSource() {
+		return source;
+	}
+
+	/**
+	 * Retourne la langue du documennt
+	 * @return String
+	 */
+	public String getLangue() {
+		return langue;
+	}
+
+	/**
+	 * Retourne le genre du document
+	 * @return String
+	 */
+	public String getGenre() {
+		return genre;
+	}
+
+	/**
+	 * Pose un verrou sur le document à partir d'un programme
+	 * @param idProg
+	 */
+	public void poserVerrou(String idProg) {
+		verrous.addElement(idProg);
+		compteur_verrou = compteur_verrou + 1;
+		System.out.println("Document verrouillé : " + this.id);
 		System.out.println("Le compteur de verrou = " + compteur_verrou);
-    }
-	
-	
-	
-	
-	
-	
-	
-	
-	public String GetEtat() {        
-        // your code here
-        return null;
-    }
-	
-	public java.util.Date GetDate_Céation() {        
-        // your code here
+	}
+
+	/**
+	 * Enlève un verrou sur le document à partir d'un programme
+	 * @param idProg
+	 */
+	public void deverouillerDocument(String idProg) {
+		verrous.remove(idProg);
+		compteur_verrou = compteur_verrou - 1;
+		System.out.println("Document déverrouillé : " + this.id);
+		System.out.println("Le compteur de verrou = " + compteur_verrou);
+	}
+
+	/**
+	 * Retourne l'état du document
+	 * @return String
+	 */
+	public String getEtat() {
+		return etat;
+	}
+
+	/**
+	 * Retourne la date de création du document
+	 * @return Date
+	 */
+	public java.util.Date GetDate_Céation() {
+		// your code here
 		return null;
-    }	
-	
-	
-	public void NommerFichier(String Fichier) {        
-        // your code here
-    } 
+	}
 
-	public void InsertionInfos(String Id_Doc, String Titre, java.util.Date Date_Creation, String Source, String Langue, String Genre) {        
-        // your code here
-    } 
+	/**
+	 * Attribue un nom au fichier
+	 * @param fichier
+	 */
+	public void nommerFichier(String fichier) {
+		// your code here
+	}
 
-	public Boolean EnLecture(String Etat) {        
-        // your code here
-        return null;
-    } 
+	/**
+	 * Insère les informations du document
+	 * @param id
+	 * @param titre
+	 * @param dateCreation
+	 * @param source
+	 * @param langue
+	 * @param genre
+	 */
+	public void insertionInfos(String id, String Ttitre,
+			java.util.Date dateCreation, String source, String langue,
+			String genre) {
+		// your code here
+	}
 
-	public Boolean EstProgramme(String Etat) {        
-        // your code here
-        return null;
-    } 
+	public boolean enLecture(String Etat) {
+		// your code here
+		return false;
+	}
 
-	public void Supprimer() {        
-        // your code here
-    } 
+	public boolean estProgramme(String Etat) {
+		// your code here
+		return false;
+	}
 
-	public Boolean Suppression() {        
-        // your code here
-        return null;
-    } 
+	public void supprimer() {
+		// your code here
+	}
 
-	public Boolean SupprimerInfos() {        
-        // your code here
-        return null;
-    } 
+	public boolean suppression() {
+		// your code here
+		return false;
+	}
 
-	public Boolean Modifier(String Id_Doc, String Titre, java.util.Date Date_Creation, String Source, String Langue, String Genre) {        
-        // your code here
-        return null;
-    } 
+	public boolean supprimerInfos() {
+		// your code here
+		return false;
+	}
 
-	public Boolean MajInfos(String Id_Doc, String Titre, java.util.Date Date_Creation, String Source, String Langue, String Genre) {        
-        // your code here
-        return null;
-    } 
+	public boolean modifier(String Id_Doc, String Titre,
+			java.util.Date Date_Creation, String Source, String Langue,
+			String Genre) {
+		// your code here
+		return false;
+	}
 
-	public void Selectionner() {        
-        // your code here
-    } 
+	public boolean majInfos(String Id_Doc, String Titre,
+			java.util.Date Date_Creation, String Source, String Langue,
+			String Genre) {
+		// your code here
+		return false;
+	}
 
-	public void GetProgrammesArchivés() {        
-        // your code here
-    } 
+	public void selectionner() {
+		// your code here
+	}
 
-	
-	public void CompterVerrouProgramme(int Verrou) {        
-        // your code here
-    } 
+	public void getProgrammesArchives() {
+		// your code here
+	}
 
-	 
+	public void compterVerrouProgramme(int Verrou) {
+		// your code here
+	}
 
-	public void AjouterProgramme(String Id_Prog) {        
-        // your code here
-    } 
+	public void ajouterProgramme(String idProg) {
+		// your code here
+	}
 
-	public void EnleverProgramme(String Id_Prog) {        
-        // your code here
-    } 
- }
+	public void enleverProgramme(String idProg) {
+		// your code here
+	}
+}
