90bb2e40e56b285d8fb2530e977f1512f5a0ddce
pbollard <pbollard>
2006-04-09 14:28:21 +0000

    connexion d'un utilisateur

--- NetJukeBox/src/proto/serveur/Utilisateur.java
+++ NetJukeBox/src/proto/serveur/Utilisateur.java
@@ -1,357 +1,344 @@
 package proto.serveur;
 
-
-
 /**
- * <p></p>
- * 
+ * Utlisateur du Net-JukeBox
  */
 public class Utilisateur {
 
-/**
- * <p>Represents ...</p>
- * 
- */
-    public String Login;
+// ATTRIBUTS
+//**********************************************
+	/**
+	 * Login : nom du compte utilisateur
+	 */
+	public String login;
 
-/**
- * <p>Represents ...</p>
- * 
- */
-    public String Nom;
+	/**
+	 * Mot de passe du compte utilisateur
+	 */
+	public String pwd;
 
-/**
- * <p>Represents ...</p>
- * 
- */
-    public String Prenom;
+	/**
+	 * Nom de l'utilisateur
+	 */
+	public String nom;
 
-/**
- * <p>Represents ...</p>
- * 
- */
-    public String Mail;
+	/**
+	 * Prénom de l'utilisateur
+	 */
+	public String prenom;
 
-/**
- * <p>Represents ...</p>
- * 
- */
-    public String Pwd;
+	/**
+	 * Adresse email de l'utilisateur
+	 */
+	public String mail;
 
-/**
- * <p>Represents ...</p>
- * 
- */
-    public java.util.List Pays;
-/**
- * <p></p>
- * 
- */
-    public Attribuer attribuer;
+	/**
+	 * Pays de l'utilisateur
+	 */
+	public String pays;
 
-/**
- * <p>Does ...</p>
- * 
- * 
- * @return 
- */
-    public Boolean Deconnecter() {        
-        // your code here
-        return null;
-    } 
+	/**
+	 * #DEFINITION#
+	 */
+	public Attribuer attribuer;
 
-/**
- * <p>Does ...</p>
- * 
- */
-    public void Deconnexion() {        
-        // your code here
-    } 
+// METHODES STATIQUES
+//*************************************************
 
-/**
- * <p>Represents ...</p>
- * 
- * 
- * @param Login 
- * @param Pwd 
- * @return 
- */
-    public boolean Connecter(String Login, String Pwd) {        
-        // your code here
-        return false;
-    } 
+	/**
+	 * Vérifie l'existence du couple Login/Pwd
+	 * @param pwd
+	 * @param login
+	 */
+	public static boolean VérifierPwd(String pwd, String login) {
+		//*************
+		// => LDAP <=
+		//*************
+		
+		return (pwd=="toto" && login=="toto");
+	}
+	
+// METHODES DYNAMIQUES
+//*************************************************
+	
+	/**
+	 * Constructeur
+	 */
+	public Utilisateur() {
+	}
+	
+	/**
+	 * Constructeur
+	 */
+	public Utilisateur(String login) {
+		super();
+		
+		//************
+		// => LDAP <=
+		//************
+		
+		this.login = login;
+		this.pwd = "toto";
+		this.nom="toto";
+		this.prenom="toto";
+		this.mail="toto@netjukebox.net";
+		this.pays="France";
+	}
+	
+	/**
+	 * Deconnecterl'utilisateur
+	 * @return
+	 */
+	public Boolean Deconnecter() {
+		// your code here
+		return null;
+	}
 
-/**
- * <p>Does ...</p>
- * 
- * 
- * @param Pwd 
- * @param Login 
- */
-    public void VérifierPwd(String Pwd, String Login) {        
-        // your code here
-    } 
+	/**
+	 * Deconnexion de l'utilisateur
+	 */
+	public void Deconnexion() {
+		// your code here
+	}
 
-/**
- * <p>Does ...</p>
- * 
- * 
- * @param Nom 
- * @param Prenom 
- * @param Mail 
- * @param Pwd 
- * @param Pays 
- * @return 
- */
-    public Boolean Inscrire(String Nom, String Prenom, String Mail, String Pwd, String Pays) {        
-        // your code here
-        return null;
-    } 
+	/**
+	 * Connecter l'utilisateur
+	 * @param Login
+	 * @param Pwd
+	 * @return
+	 */
+	public boolean Connecter(String Login, String Pwd) {
+		// your code here
+		return false;
+	}
 
-/**
- * <p>Does ...</p>
- * 
- * 
- * @param Nom 
- * @param Prenom 
- * @param Mail 
- * @param Pwd 
- * @param Pays 
- * @return 
- */
-    public Boolean Inscription(String Nom, String Prenom, String Mail, String Pwd, java.util.List Pays) {        
-        // your code here
-        return null;
-    } 
+	/**
+	 * Connexion de l'utilisateurs
+	 * @return
+	 */
+	public boolean Connexion() {
+		// your code here
+		return false;
+	}
 
-/**
- * <p>Does ...</p>
- * 
- */
-    public void SupprimerInfos() {        
-        // your code here
-    } 
+	/**
+	 * Inscrire l'utilisateur
+	 * @param Nom
+	 * @param Prenom
+	 * @param Mail
+	 * @param Pwd
+	 * @param Pays
+	 * @return
+	 */
+	public Boolean Inscrire(String Nom, String Prenom, String Mail, String Pwd,
+			String Pays) {
+		// your code here
+		return null;
+	}
 
-/**
- * <p>Does ...</p>
- * 
- * 
- * @return 
- */
-    public boolean Connecter() {        
-        // your code here
-        return false;
-    } 
+	/**
+	 * Inscription de l'utilisateur
+	 * @param Nom
+	 * @param Prenom
+	 * @param Mail
+	 * @param Pwd
+	 * @param Pays
+	 * @return
+	 */
+	public Boolean Inscription(String Nom, String Prenom, String Mail,
+			String Pwd, java.util.List Pays) {
+		// your code here
+		return null;
+	}
 
-/**
- * <p>Does ...</p>
- * 
- */
-    public void ModifierInfos() {        
-        // your code here
-    } 
+	/**
+	 * Supprimer l'utilisateur
+	 * @param Login
+	 */
+	public void SupprimerUtilisateur(String Login) {
+		// your code here
+	}
+	
+	/**
+	 * Supprimer les informations relatives à l'utilisateur
+	 */
+	public void SupprimerInfos() {
+		// your code here
+	}
 
-/**
- * <p>Does ...</p>
- * 
- * 
- * @param Login 
- */
-    public void SupprimerUtilisateur(String Login) {        
-        // your code here
-    } 
 
-/**
- * <p>Does ...</p>
- * 
- * 
- * @param Id_Perm 
- * @param Lib_Perm 
- * @return 
- */
-    public Boolean SetPermission(int Id_Perm, String Lib_Perm) {        
-        // your code here
-        return null;
-    } 
 
-/**
- * <p>Does ...</p>
- * 
- * 
- * @return 
- */
-    public Boolean VérifConnecté() {        
-        // your code here
-        return null;
-    } 
+	/**
+	 * Modifier les informations de l'utilisateur
+	 */
+	public void ModifierInfos() {
+		// your code here
+	}
 
-/**
- * <p>Does ...</p>
- * 
- */
-    public void supprimer() {        
-        // your code here
-    } 
+	
 
-/**
- * <p>Does ...</p>
- * 
- * 
- * @return 
- */
-    public String GetLogin() {        
-        // your code here
-        return null;
-    } 
+	/**
+	 * Attribuer une permission à l'utilisateur
+	 * @param Id_Perm
+	 * @param Lib_Perm
+	 * @return
+	 */
+	public Boolean SetPermission(int Id_Perm, String Lib_Perm) {
+		// your code here
+		return null;
+	}
 
-/**
- * <p>Does ...</p>
- * 
- * 
- * @return 
- */
-    public String GetNom() {        
-        // your code here
-        return null;
-    } 
+	/**
+	 * Vérification de l'état de connexion de l'utilisateur
+	 * @return
+	 */
+	public Boolean VérifConnecté() {
+		// your code here
+		return null;
+	}
 
-/**
- * <p>Does ...</p>
- * 
- * 
- * @return 
- */
-    public String GetPrenom() {        
-        // your code here
-        return null;
-    } 
+	/**
+	 * Supprimer l'utilisateur
+	 */
+	public void supprimer() {
+		// your code here
+	}
 
-/**
- * <p>Does ...</p>
- * 
- * 
- * @return 
- */
-    public String GetMail() {        
-        // your code here
-        return null;
-    } 
+	/**
+	 * Retourne le login de l'utilisateur
+	 * @return
+	 */
+	public String GetLogin() {
+		// your code here
+		return null;
+	}
 
-/**
- * <p>Does ...</p>
- * 
- * 
- * @return 
- */
-    public String GetPwd() {        
-        // your code here
-        return null;
-    } 
+	/**
+	 * Retourne le nom de l'utilisateur
+	 * @return
+	 */
+	public String GetNom() {
+		// your code here
+		return null;
+	}
 
-/**
- * <p>Does ...</p>
- * 
- * 
- * @return 
- */
-    public String GetPays() {        
-        // your code here
-        return null;
-    } 
+	/**
+	 * Retourne le prénom de l'utilisateur
+	 * @return
+	 */
+	public String GetPrenom() {
+		// your code here
+		return null;
+	}
 
-/**
- * <p>Does ...</p>
- * 
- * 
- * @return 
- */
-    public Boolean EstConnecté() {        
-        // your code here
-        return null;
-    } 
+	/**
+	 * Retourne l'adresse email de l'utilisateur
+	 * @return
+	 */
+	public String GetMail() {
+		// your code here
+		return null;
+	}
 
-/**
- * <p>Represents ...</p>
- * 
- */
-    public void Selectionner() {        
-        // your code here
-    } 
+	/**
+	 * Retourne le mot de passe de l'utilisateur
+	 * @return
+	 */
+	public String GetPwd() {
+		// your code here
+		return null;
+	}
 
-/**
- * <p>Does ...</p>
- * 
- * 
- * @param Nom 
- * @param Prenom 
- * @param Mail 
- * @param Pwd 
- * @param Pays 
- * @return 
- */
-    public Boolean Modifier(String Nom, String Prenom, String Mail, String Pwd, String Pays) {        
-        // your code here
-        return null;
-    } 
+	/**
+	 * Retourne le pays de l'utilisateur
+	 * @return
+	 */
+	public String GetPays() {
+		// your code here
+		return null;
+	}
 
-/**
- * <p>Does ...</p>
- * 
- * 
- * @param Nom 
- * @param Prenom 
- * @param Mail 
- * @param Pwd 
- * @param Pays 
- * @return 
- */
-    public Boolean MajInfos(String Nom, String Prenom, String Mail, String Pwd, String Pays) {        
-        // your code here
-        return null;
-    } 
+	/**
+	 * Vérification de l'état de connexion de l'utilisateur
+	 * @return
+	 */
+	public Boolean EstConnecté() {
+		// your code here
+		return null;
+	}
 
-/**
- * <p>Does ...</p>
- * 
- */
-    public void GetPermissions() {        
-        // your code here
-    } 
+	/**
+	 * Sélectionne l'utilisateur
+	 */
+	public void Selectionner() {
+		// your code here
+	}
 
-/**
- * <p>Does ...</p>
- * 
- * 
- * @param Id_Perm 
- * @return 
- */
-    public Boolean ajouterPermission(int Id_Perm) {        
-        // your code here
-        return null;
-    } 
+	/**
+	 * Modifier l'utilisateur
+	 * @param Nom
+	 * @param Prenom
+	 * @param Mail
+	 * @param Pwd
+	 * @param Pays
+	 * @return
+	 */
+	public Boolean Modifier(String Nom, String Prenom, String Mail, String Pwd,
+			String Pays) {
+		// your code here
+		return null;
+	}
 
-/**
- * <p>Does ...</p>
- * 
- * 
- * @param Id_Perm 
- * @return 
- */
-    public Boolean RetirerPermission(int Id_Perm) {        
-        // your code here
-        return null;
-    } 
+	/**
+	 * Mise à jour des informations de l'utilisateur
+	 * @param Nom
+	 * @param Prenom
+	 * @param Mail
+	 * @param Pwd
+	 * @param Pays
+	 * @return
+	 */
+	public Boolean MajInfos(String Nom, String Prenom, String Mail, String Pwd,
+			String Pays) {
+		// your code here
+		return null;
+	}
 
-/**
- * <p>Does ...</p>
- * 
- * 
- * @param Id_Perm 
- * @param Lib_Perm 
- * @return 
- */
-    public Boolean DefinirPermission(int Id_Perm, String Lib_Perm) {        
-        // your code here
-        return null;
-    } 
- }
+	/**
+	 * Retourne les permissions attribuées à l'utilisateur
+	 */
+	public void GetPermissions() {
+		// your code here
+	}
+
+	/**
+	 * Ajouter une permision à l'utilisateur
+	 * @param Id_Perm
+	 * @return
+	 */
+	public Boolean ajouterPermission(int Id_Perm) {
+		// your code here
+		return null;
+	}
+
+	/**
+	 * Retirer une permission à l'utilisateur 
+	 * @param Id_Perm
+	 * @return
+	 */
+	public Boolean RetirerPermission(int Id_Perm) {
+		// your code here
+		return null;
+	}
+
+	/**
+	 * Définir une permission
+	 * @param Id_Perm
+	 * @param Lib_Perm
+	 * @return
+	 */
+	public Boolean DefinirPermission(int Id_Perm, String Lib_Perm) {
+		// your code here
+		return null;
+	}
+}
