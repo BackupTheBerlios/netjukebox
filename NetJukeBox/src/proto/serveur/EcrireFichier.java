6882da3c7c4b3837ff266915a32a73a4889c6b60
dominique <dominique>
2006-04-13 17:40:14 +0000

    Rajout du chemin de sauvegarde - Pb encore sur le test d'existence du fichier

--- NetJukeBox/src/proto/serveur/EcrireFichier.java
+++ NetJukeBox/src/proto/serveur/EcrireFichier.java
@@ -1,18 +1,30 @@
 package proto.serveur;
+
 import java.io.*;
 
 public class EcrireFichier {
-String destination;
-String donnee;
+	String nom_fichier;
+	String donnee;
+	File fichier = null;
 
-	public EcrireFichier(String destination, String donnee) {
-		this.destination = destination;
+	public EcrireFichier(String nom_fichier, String donnee) {
+		this.nom_fichier = nom_fichier;
 		this.donnee = donnee;
+		String chemin = "/home/admindg/workspace/NetJukeBox/";
 			try {
-				BufferedWriter fichier = new BufferedWriter(new FileWriter(destination, true));
-				fichier.write(donnee);
-				fichier.newLine();
-				fichier.close();
+				if (fichier == null){ //&& fichier.isFile()){
+					File fichier = new File(chemin + nom_fichier);
+					BufferedWriter ecrire = new BufferedWriter(new FileWriter(fichier));
+					ecrire.write(donnee);
+					ecrire.newLine();
+					ecrire.close();
+				} else {
+					//fichier = new File(chemin + nom_fichier);
+					BufferedWriter ecrire = new BufferedWriter(new FileWriter(nom_fichier));
+					ecrire.write(donnee);
+					ecrire.newLine();
+					ecrire.close();
+				}
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
