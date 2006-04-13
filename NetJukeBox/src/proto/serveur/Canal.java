e3a2a96d51f14effe5368eb2f1d1f7a91e9c5d30
dominique <dominique>
2006-04-13 17:48:46 +0000

    suite erreur de compil

--- NetJukeBox/src/proto/serveur/Canal.java
+++ NetJukeBox/src/proto/serveur/Canal.java
@@ -101,26 +101,6 @@ public class Canal {
 		//Si le RTPServer existe, on le stoppe
 		if (this.RTP != null) this.RTP.stop();
 	}
-
-	/**
-	 * Création du RTPServer
-	 * @param ip
-	 * @param port
-	 */
-	public void createRTPServer(String ip, int port) {
-		
-		//Si le RTPServer n'existe pas, on le crée
-		if (this.RTP == null) this.RTP = new RTPServer(ip, port);
-	}
-	
-	/**
-	 * Arrête la diffusion
-	 */
-	public void stopDiffusion() {
-		
-		//Si le RTPServer existe, on le stoppe
-		if (this.RTP != null) this.RTP.stop();
-	}
 	
 	/**
 	 * Création du canal ==> CONSTRUCTEUR ???
