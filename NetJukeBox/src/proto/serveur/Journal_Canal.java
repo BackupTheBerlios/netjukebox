2c4d54abcfd9d4fde07f8855b55ebb1437d09e1e
dominique <dominique>
2006-04-13 17:39:14 +0000

    Ebauche

--- NetJukeBox/src/proto/serveur/Journal_Canal.java
+++ NetJukeBox/src/proto/serveur/Journal_Canal.java
@@ -1,5 +1,9 @@
 package proto.serveur;
 
+import java.io.*;
+import java.text.*;
+import java.util.Date;
+
 
 /**
  * <p></p>
@@ -19,20 +23,45 @@ public class Journal_Canal {
  */
     public java.util.Date date_jourcanal;
 
-/**
- * <p>Does ...</p>
- * 
- */
-    public void creer() {        
-        // your code here
+    /**
+     * <p>Does ...</p>
+     * @throws IOException 
+     * 
+     */
+    public void creer(String idCanal) throws IOException {        
+    	Date date = new Date();
+    	SimpleDateFormat formateur = new SimpleDateFormat("dd-MM-yyyy");
+    	//System.out.println("date : " + date.toString());
+    	//System.out.println("date formatée : " + formateur.format(date));
+    	String nom_journal = (idCanal + "_" + formateur.format(date) + ".txt");
+    	System.out.println("Le journal s'appelle : " + nom_journal);
+    	new EcrireFichier(nom_journal, "toto");
     } 
 
+
+    public static void main (String[] args) throws IOException {
+    	Journal_Canal j = new Journal_Canal();
+    	j.creer("C1");
+    }
+    
+    
 /**
  * <p>Does ...</p>
  * 
  */
-    public void archiver() {        
-        // your code here
+    public void archiver(String idCanal, String date, String idProg, int Nbconnect) {        
+    	int i;
+		String donnee, fichier_archive;
+		// Création du nom complet du fichier Archive de tous les documents
+		// composant le programme
+		
+		// Sauvegarde de la liste des documents du programme dans le fichier
+		// Archive
+		for (i = 0; i < this.documents.size(); i++) {
+			donnee = (String) this.documents.elementAt(i);
+			new EcrireFichier(fichier_archive, donnee);
+		}
+			System.out.println("Le programme est archivé dans le fichier : "+ fichier_archive);
     } 
 
 /**
@@ -43,3 +72,4 @@ public class Journal_Canal {
         // your code here
     } 
  }
+
