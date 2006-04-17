d4e7990e905dd720bff50cb9f55bcd0837b5541a
dominique <dominique>
2006-04-17 18:51:17 +0000

    *** empty log message ***

--- NetJukeBox/src/proto/serveur/TestJdbc.java
+++ NetJukeBox/src/proto/serveur/TestJdbc.java
@@ -1,9 +1,10 @@
 package proto.serveur;
 
 import java.sql.SQLException;
-import java.util.Hashtable;
+//import java.util.Hashtable;
 import java.util.Dictionary;
 import java.util.Enumeration;
+import java.util.Vector;
 
 public class TestJdbc {
 	static String requete;
@@ -13,18 +14,21 @@ public class TestJdbc {
 		String tel = "0000000000";	
 		String requete = "INSERT INTO Annuaire VALUES ('" + nom + "', '" + prenom + "', '" +tel + "');"; 
 		//new Jdbc(requete);
-		String requete1 = "SELECT * FROM Annuaire;";// WHERE nom = 'toto';";
+		String requete1 = "SELECT * FROM Annuaire WHERE nom = 'papa';";
 		new Jdbc(requete1);
-		Dictionary dico = Jdbc.retour();
-
-		//DICTIONARY : Affichage des données du dictionnaire
-		Enumeration donnee = dico.elements();
-		Enumeration colonne = dico.keys();
-		for(int i = 0; i < dico.size(); i++){
-			//Enumeration donnee = dico.elements();
-			//Enumeration colonne = dico.keys();
-			System.out.print(colonne.nextElement() + " : "); 
-			System.out.println(donnee.nextElement());
+		Vector v = Jdbc.retour();
+		for (int j = 0; j < v.size(); j++) {
+		
+			Dictionary dico = (Dictionary) v.elementAt(j);
+			//DICTIONARY : Affichage des données du dictionnaire
+			Enumeration donnee = dico.elements();
+			Enumeration colonne = dico.keys();
+			for(int i = 0; i < dico.size(); i++){
+				//Enumeration donnee = dico.elements();
+				//Enumeration colonne = dico.keys();
+				System.out.print(colonne.nextElement() + " : "); 
+				System.out.println(donnee.nextElement());
+			}
 		}
 	}
 }	
