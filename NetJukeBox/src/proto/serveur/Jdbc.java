274ce2ed5ed42b435d31180def89a483bb0c5a6b
dominique <dominique>
2006-04-17 17:01:31 +0000

    version exécutant la requete et retournant les infos dans un tableau

--- NetJukeBox/src/proto/serveur/Jdbc.java
+++ NetJukeBox/src/proto/serveur/Jdbc.java
@@ -1,6 +1,9 @@
 package proto.serveur;
 
 import java.sql.*;
+import java.util.Dictionary;
+import java.util.Enumeration;
+import java.util.Hashtable;
 
 public class Jdbc {
 	//Connexion a la base
@@ -9,6 +12,8 @@ public class Jdbc {
 	Statement state;
 	//Permet la recuperation des donnees
 	private ResultSet rs;
+	//Création d'un dictionnaire retournant les données 
+    static Dictionary dico = new Hashtable();
 	
 	//Initialise les instances necessaires a la connexion a la base
 	private void connectToDB(String requete) {
@@ -17,25 +22,34 @@ public class Jdbc {
 			Statement state = connect.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
 			rs = state.executeQuery(requete);
 			ResultSetMetaData rsmd = rs.getMetaData();
-			  int colomnCount = rsmd.getColumnCount();
-
+			int colomnCount = rsmd.getColumnCount();
 			while (rs.next()) {
 				for (int i = 1; i <= colomnCount; i++) {
-					System.out.print(rs.getObject(i) + "\t");
+					//Récupération du nom de la colonne
+					String colonne = (String) rsmd.getColumnName(i);
+					//Récupération de la donnée
+					String donnee = (String) rs.getObject(i);
+					//Création du dictionnaire à retourner à l'objet
+					dico.put(colonne, donnee);				
 				}
 			}
+			retour();
 		} 
 		catch (Exception e){
 			System.out.println("connectToDB" + e);
 		}
 	}
 
+	static Dictionary retour() {
+		return dico;	
+	}
+	
 	private void initConnection (String driver, String url, String login, String password) {
 		try {
 			Class.forName(driver);
 			connect = DriverManager.getConnection(url, login, password);
 			connect.setAutoCommit(false);
-			System.out.println("connexion reussie !!!");
+			//System.out.println("connexion reussie !!!");
 		}
 		catch (ClassNotFoundException e) {
 			System.out.println("erreur du chargement du pilote JDBC : \n" + e);
@@ -49,7 +63,7 @@ public class Jdbc {
 	public void Commitmodif() {
 		try {
 			connect.commit();
-			System.out.println("Commit effectué !!!");
+			//System.out.println("Commit effectué !!!");
 		}
 		catch (Exception e){
 			System.out.println("Commitmodif " + e);
