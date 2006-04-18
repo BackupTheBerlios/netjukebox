023218eec274d2cee5ac4c8264ac653709cc20ec
dominique <dominique>
2006-04-18 17:56:09 +0000

    *** empty log message ***

--- NetJukeBox/src/proto/serveur/TestJdbc.java
+++ NetJukeBox/src/proto/serveur/TestJdbc.java
@@ -9,26 +9,27 @@ import java.util.Vector;
 public class TestJdbc {
 	static String requete;
 	public static void main (String args[]) throws SQLException {
-		String nom = "papa";
-		String prenom = "pupu";
-		String tel = "0000000000";	
-		String requete = "INSERT INTO Annuaire VALUES ('" + nom + "', '" + prenom + "', '" +tel + "');"; 
-		//new Jdbc(requete);
-		String requete1 = "SELECT * FROM Annuaire WHERE nom = 'papa';";
-		new Jdbc(requete1);
-		Vector v = Jdbc.retour();
-		for (int j = 0; j < v.size(); j++) {
+		String nom = "pipi";
+		String prenom = "pépé";
+		String tel = "111111";	
+		//String requete = "INSERT INTO Annuaire VALUES ('" + nom + "', '" + prenom + "', '" +tel + "');"; 
+		Jdbc base = Jdbc.getInstance();
+		base.openDB("org.postgresql.Driver", "jdbc:postgresql://192.168.0.2:5432/NetJukeBox", "postgres", "postgres");
+		//base = Jdbc.getInstance();
+		//base.executeUpdate(requete);
+		String requete1 = "SELECT nom FROM Annuaire WHERE nom = 'papa';";
+		Vector resultats = base.executeQuery(requete1);
 		
-			Dictionary dico = (Dictionary) v.elementAt(j);
+		for (int j = 0; j < resultats.size(); j++) {
+			Dictionary dico = (Dictionary) resultats.elementAt(j);
+			System.out.println(dico.get("nom"));
 			//DICTIONARY : Affichage des données du dictionnaire
-			Enumeration donnee = dico.elements();
-			Enumeration colonne = dico.keys();
-			for(int i = 0; i < dico.size(); i++){
-				//Enumeration donnee = dico.elements();
-				//Enumeration colonne = dico.keys();
-				System.out.print(colonne.nextElement() + " : "); 
-				System.out.println(donnee.nextElement());
-			}
+			//Enumeration donnee = dico.elements();
+			//Enumeration colonne = dico.keys();
+			//for(int i = 0; i < dico.size(); i++){
+				//System.out.print(colonne.nextElement() + " : "); 
+				//System.out.println(donnee.nextElement());
+			//}
 		}
 	}
 }	
