5c337ee4b76b9f145434026c2b9c6c5e97640343
marie <marie>
2006-05-25 17:43:48 +0000

    avec synchronisation

--- NetJukeBox/src/proto/serveur/TransformeDate.java
+++ NetJukeBox/src/proto/serveur/TransformeDate.java
@@ -3,15 +3,23 @@ package proto.serveur;
 import java.util.Date;
 import java.text.SimpleDateFormat;
 
+import org.apache.log4j.Logger;
+
 public class TransformeDate {
+	/**
+	 *  Crée le logger de la classe
+	 */
+	private static final Logger logger = Logger.getLogger(TransformeDate.class);
+	
 	public Date d;
 
 	public TransformeDate(String sDate) {
+		logger.debug("Démarrage: TransformeDate");
 		try {
 			d = stringToDate(sDate, "dd-MM-yyyy");
 		} catch(Exception e) {
-			System.err.println("Exception :");
-			e.printStackTrace();
+			logger.error("Exception :", e);
+			logger.debug("Arrêt: TransformeDate");
 		}
 	}
 
