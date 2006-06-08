8e146d9210bfdc4442f548ed65086aec361f58b9
marie <marie>
2006-06-08 12:20:45 +0000

    *** empty log message ***

--- NetJukeBox/src/proto/serveur/TestLdap.java
+++ NetJukeBox/src/proto/serveur/TestLdap.java
@@ -2,20 +2,21 @@ package proto.serveur;
 
 import java.io.IOException;
 import java.util.Dictionary;
-import java.util.Enumeration;
+import java.util.Hashtable;
 import java.util.Vector;
 
-import javax.naming.NameClassPair;
 import javax.naming.NamingException;
 import javax.naming.directory.Attribute;
 import javax.naming.directory.Attributes;
+
 
 public class TestLdap {
 		
 	@SuppressWarnings("static-access")
 	public static void main(String[] args) throws NamingException, IOException {
 		Ldap ldap = Ldap.getInstance();
-		ldap.openLdap("com.sun.jndi.ldap.LdapCtxFactory","ldap://55.24.8.10:389/dc=netjukebox,dc=com","simple", "admin", "admin","mot2passe");
+		ldap.openLdap("com.sun.jndi.ldap.LdapCtxFactory", "ldap://192.168.0.2:389/dc=netjukebox,dc=com", "simple",  "bollard", "phix", "dc=netjukebox,dc=com");
+		//ldap.openLdap("com.sun.jndi.ldap.LdapCtxFactory","ldap://192.168.0.2:389/dc=netjukebox,dc=com","simple", "bollard","respprog","phix");
 		//ldap.executeSupprimer("bollard");
 		//ldap.executeCreer("new", "new", "new", "new", "netjukebox@gmail.com", "Pays", "usager");
 		//ldap.executeCreer("login", "password", "toto", "titi", "toto@gmail.com", "france", "usager");
@@ -25,16 +26,44 @@ public class TestLdap {
 		//ldap.ModifieAttributs("mail", "new@gmail", "login", "admin");
 		
 //CODE POUR LE LISTAGE DES GROUPES
-	     	 Vector vect= ldap.ListeGroupe();
-	         for(int i=0; i < vect.size(); i++)
-	            if(vect.elementAt(i) != null)
-	               System.out.println (vect.elementAt(i));
-	            else System.out.println("vect[" + i + "] est null");
-	         
-//CODE POUR LA SUPPRESSION D'UN GROUPE
+ 
+
+	
+	    
+//	       CODE POUR LE LISTAGE DES GROUPES
+
+	    Vector vect1= ldap.listerUtilisateur();
+	      for(int i=0; i < vect1.size(); i++)
+		            if(vect1.elementAt(i) != null){
+		            	Attributes dico = (Attributes)vect1.elementAt(i);
+		            	Attribute uidAtt = (Attribute) dico.get("uid");
+		    			String uid = (String)uidAtt.get();
+		    			System.out.println(uid);
+		    			Attribute snAtt = (Attribute) dico.get("sn");
+		    			String sn = (String)snAtt.get();
+		    			System.out.println(sn);
+		    			Attribute givenNameAtt = (Attribute) dico.get("givenName");
+		    			String givenName = (String)givenNameAtt.get();
+		    			System.out.println(givenName);
+		    			Attribute mailAtt = (Attribute) dico.get("mail");
+		    			String mail = (String)mailAtt.get();
+		    			System.out.println(mail);
+		    			Attribute stAtt = (Attribute) dico.get("st");
+		    			String st = (String)stAtt.get();
+		    			System.out.println(st);
+		    			Attribute ouAtt = (Attribute) dico.get("ou");
+		    			String ou = (String)ouAtt.get();
+		    			System.out.println(ou);
+	     	 			}
+	     	else System.out.println("vect[" + i + "] est null");
+	
+			
+	
+
+//CODE POUR LA SUPP	ESSION D'UN GROUPE
 				
-			ldap.CreationGroupe("test");
-			ldap.SupprimerGroupe("test");
+		//	ldap.CreationGroupe("test");
+		//	ldap.SupprimerGroupe("test");
 	        
 	         /**if(vect.elementAt(1) != null){
 	              vect.set(1, new Integer(1000));
